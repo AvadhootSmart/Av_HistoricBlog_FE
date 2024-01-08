ac2b7ab55a9c25da512140467140ad48e9f8b915
AvadhootSmart <1234avadhootsmart@gmail.com>
2024-01-08 17:39:24 +0530

    commits

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,18 +1,34 @@
 const btn_open = document.querySelectorAll(".open");
 
-btn_open.forEach(function(button){
-    button.addEventListener('mouseover', function(){
-        const card = this.parentElement.nextElementSibling;
-        card.style.opacity = 0;
+btn_open.forEach(function (button) {
+  button.addEventListener("mouseover", function () {
+    const card = this.parentElement.nextElementSibling;
+    card.style.opacity = 0;
+  });
+});
 
-    })
-})
+btn_open.forEach(function (button) {
+  button.addEventListener("mouseleave", function () {
+    const card = this.parentElement.nextElementSibling;
+    card.style.opacity = 1;
+  });
+});
 
-btn_open.forEach(function(button){
-    button.addEventListener('mouseleave', function(){
-        const card = this.parentElement.nextElementSibling;
-        card.style.opacity = 1;
-    })
-})
+//for mobile:
 
+function TextEffect() {
+  if (!isImg) {
+    const card = this.parentElement.nextElementSibling;
+    card.style.opacity = 0;
+    isImg = true;
+  } else {
+    const card = this.parentElement.nextElementSibling;
+    card.style.opacity = 1;
+    isImg = false;
+  }
+}
 
+let isImg = false;
+btn_open.forEach(function (button) {
+  button.addEventListener("click", TextEffect);
+});
